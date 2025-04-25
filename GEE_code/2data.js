6fc3071b36f0be80bb58f56dbd8be7d0cea873c0
Evelyn Wu <Van_Wu1@outlook.com>
2025-04-25 01:20:31 +0100

    ndvi数据部分测试（变化等于0头大了

--- GEE_code/2data.js
+++ GEE_code/2data.js
@@ -21,16 +21,9 @@ var boroughStyledContent = boroughRegion.style({
 
 /// ===== [Xinyi Zeng] Begin: NDVI EXAMPLE 可视化失败版本 =====
 // 评论：其实也还可以，看得出雏形了（V）
-var modisNDVI = ee.ImageCollection("MODIS/006/MOD13Q1")
-  .filterBounds(defaultRegion)
-  .select("NDVI");
-
 function getNDVIImageByYear(year) {
-  return modisNDVI
-    .filter(ee.Filter.calendarRange(year, year, 'year'))
-    .max()
-    .multiply(0.0001)  // normalize to 0–1
-    .clip(defaultRegion);
+  var assetPath = 'projects/casa0025geeappglaicier/assets/NDVI/NDVI_' + year;
+  return ee.Image(assetPath).clip(defaultRegion);
 }
 
 function getSentinel2Filtered(year, cloudThreshold) {
@@ -47,10 +40,6 @@ function getGlacierBoundary() {
 
 
 // ===== [Yifan Wu] Begin: 自定义 Water Body; 预测试 =====
-var waterbody = ee.ImageCollection("JRC/GSW1_4/YearlyHistory")
-  .filterBounds(defaultRegion)
-  .select("WaterBody");
-
 function getWaterbodyByYear(year) {
   var image = ee.ImageCollection("JRC/GSW1_4/YearlyHistory")
     .filter(ee.Filter.eq('year', year))
@@ -60,11 +49,3 @@ function getWaterbodyByYear(year) {
 }
   
 // ===== [Yifan Wu] End =====
-
-
-// 可根据项目需要扩展更多数据集加载函数，如：
-/*
-  - getPrecipitationByYear
-  - getGlacierThickness(year)
-  - getLandsat(year, cloudCover)
-*/
